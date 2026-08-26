098510ad86f928a414ee995ba70e6c9d7e9e8ab5
Personwritep <89448434+personwritep@users.noreply.github.com>
2026-08-26 19:42:16 +0900

    Update user script metadata for Shrink Emoji

--- Shrink_Emoji.user.js
+++ Shrink_Emoji.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @name        Shrink Emoji ⭐
+// @name        Shrink Emoji ⭐▢
 // @namespace        http://tampermonkey.net/
-// @version        0.1
+// @version        0.2
 // @description        アメブロ絵文字のデータを軽くする　ショートカット「F2」
 // @author        Ameba Blog User
-// @match        https://blog.ameba.jp/ucs/entry/srventry*
-// @exclude        https://blog.ameba.jp/ucs/entry/srventrylist.do*
+// @match        https://blog.ameba.jp/ucs/entry/srventryinsertinput.do*
+// @match        https://blog.ameba.jp/ucs/entry/srventryupdateinput.do*
 // @icon        https://www.google.com/s2/favicons?sz=64&domain=ameblo.jp
 // @grant        none
 // @updateURL        https://github.com/personwritep/Shrink_Emoji/raw/main/Shrink_Emoji.user.js
